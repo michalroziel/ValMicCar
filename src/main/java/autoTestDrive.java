784c667c64f6a50f658da04b6b3fbe1a880e13d8
Michał Roziel <miro00002@stud.uni-saarland.de>
2023-11-09 11:10:12 +0100

    initial commit

---
+++ src/main/java/autoTestDrive.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class autoTestDrive {
+    public static void main(String[] args) {
+    auto car1 = new auto("Volkswagen","Golf 8",240);
+    car1.drive();
+    }
+}
